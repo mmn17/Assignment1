13e16f96b54da120aa7874b3bfbc808523ea0723
Mohammed <mmn17@my.fsu.edu>
2019-02-03 20:27:55 -0500

    Updated file added SimpleHammer.cpp
    File added for a new weapon Simple Hammer

---
+++ SimpleHammer.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   SimpleHammer.cpp
+ * Author: Mohammed <mmn17@my.fsu.edu>
+ * 
+ * Created on Febuary 2, 2017, 4:20 PM
+ */
+
+#include "SimpleHammer.h"
+
+/virtual double hit(double armor){
+
+
+}
+
+
