91ab2763f614ff0a2f1b2e5f67338523c73dbcb7
Mohammed <mmn17@my.fsu.edu>
2019-02-03 20:29:19 -0500

    Updated file added Flail
    File added for a new weapon Flail

---
+++ Flail.cpp
@@ -0,0 +1,14 @@
+/* 
+ * File:   Flail.cpp
+ * Author: Mohammed <mmn17@my.fsu.edu>
+ * 
+ * Created on Febuary 2, 2017, 4:20 PM
+ */
+
+#include "CrazyRandomSword.h"
+
+virtual double hit(double armor){
+
+    
+}
+
