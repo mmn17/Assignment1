6ff5283acd18558ace123402eb475d5504a8b882
Mohammed <mmn17@my.fsu.edu>
2019-02-03 20:34:55 -0500

    Updated file added CrazyRandomSword.h
    File added for a new weapon Crazy Random Sword

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   CrazyRandomSword.cpp
+ * Author: Mohammed <mmn17@my.fsu.edu>
+ * 
+ * Created on Febuary 2, 2017, 4:20 PM
+ */
+
+#include "CrazyRandomSword.h"
+
+//define functions here
+
+ virtual double hit(double armor){
+
+ }
+
